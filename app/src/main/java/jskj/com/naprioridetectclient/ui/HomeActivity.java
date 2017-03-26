fcf6a2320af583c0f6cf9b8d0957e763a9d2c2dc
18252580292 <18252580292@163.com>
2017-03-26 20:13:55 +0800

    add page of home icon's

--- app/src/main/java/jskj/com/naprioridetectclient/ui/HomeActivity.java
+++ app/src/main/java/jskj/com/naprioridetectclient/ui/HomeActivity.java
@@ -3,15 +3,29 @@ package jskj.com.naprioridetectclient.ui;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
+import android.widget.TextView;
 
 import jskj.com.naprioridetectclient.R;
 
 public class HomeActivity extends AppCompatActivity {
+    private static final String TAG = "HomeActivity";
+    private TextView mTxtTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_home);
+        initView();
+        initData();
     }
+
+    private void initData() {
+
+    }
+
+    private void initView() {
+        mTxtTitle = (TextView) findViewById(R.id.txt_title_home);
+    }
+
 }
